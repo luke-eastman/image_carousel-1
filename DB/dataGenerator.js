1a983a99af17656a2111128a3f72adb604bc601d
Luke Eastman <LukeAEastman@gmail.com>
2020-10-14 20:30:13 -0700

    export data generator and import to seed script

--- DB/dataGenerator.js
+++ DB/dataGenerator.js
@@ -37,37 +37,5 @@ const randomUrlIndex = () => {
 }
 
 let urls = imageUrlGenerator();
-console.log(urls[200]);
 
-console.log('1 record: ', dataGenerator(1));
-console.log('5 records: ', dataGenerator(5));
-// const imageRandomizer = (array) => {
-//    var randomIndex = Math.floor(Math.random() * array.length)
-//   return array[randomIndex];
-// }
-
-
-
-// var fakeImageDataCreator = function(numberOfEnries) {
-
-//   var dataEntries = [];
-// var currentID = 2
-// var referenceIndex = 0
-//   for (var i = 0; i < numberOfEnries - 7; i++) {
-//          if (i === referenceIndex + 7) {
-//           referenceIndex += 7
-//           currentID++
-//         }
-//     dataEntries.push({
-//        product: currentID,
-//        imageName: faker.commerce.productDescription(),
-//        color: faker.commerce.color().split(' ').join('-'),
-//        url: 'https://target-image-carousel.s3-us-west-1.amazonaws.com/' + imageRandomizer(randomImageUrls),
-//        alt: faker.image.abstract()
-//     })
-//   }
-//   return dataEntries;
-// }
-
-// var fakeData = fakeImageDataCreator(700);
-// var data = realData.concat(fakeData)
+module.exports = dataGenerator;
