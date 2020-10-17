f212d0902e379120b73285d6b6247bf09b38a966
Luke Eastman <LukeAEastman@gmail.com>
2020-10-16 17:21:48 -0700

    create seed script that alterantes between creating a batch and loading to postgres db. will be replaced with a script that reads from file.

--- DB/postgresSeed.js
+++ DB/postgresSeed.js
@@ -1,9 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
-
+const dataGenerator = require('./dataGenerator.js');
 const sequelize = new Sequelize('carousel', 'postgres', 'hrr48', {
   dialect: 'postgres',
-  storage: 'localhost:5432'
-});
+  storage: 'localhost:5432',
+  logging: false
+})
+
 
 const test = async () => {
   try {
@@ -14,10 +16,9 @@ const test = async () => {
   }
 }
 
-test();
 
 const Product = sequelize.define('Product', {
-  productNumber : {
+  product : {
     type: DataTypes.BIGINT,
     allowNull: false,
     unique: true
@@ -28,7 +29,7 @@ const Product = sequelize.define('Product', {
 });
 
 const Image = sequelize.define('Image', {
-  productNumber : {
+  product_id : {
     type: DataTypes.BIGINT,
     allowNull: false,
   },
@@ -47,16 +48,59 @@ const Image = sequelize.define('Image', {
   }
 });
 
-Product.hasMany(Image, {
-  foreignKey: 'productNumber'
-});
+Product.hasMany(Image);
 Image.belongsTo(Product);
 
-(async () => {
-   await Product.sync({ alter: true });
-  const newProduct = await Product.create({
-    productNumber: 0,
-    name: 'first product!'
-  });
-  console.log(newProduct.id);
-})()
+
+
+const seedProduct = async (productNumber, productName) => {
+  try {
+    await Product.create({
+      productNumber: productNumber,
+      productName: productName
+    })
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const seedImage = async (image) => {
+  try {
+    await Image.create({
+      product_id: image.product,
+      imageName: image.imageName,
+      color: image.color,
+      url: image.url,
+      alt: image.alt
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const ENTRIES = 10000000;
+const STEP_COUNT = 100;
+
+sequelize.sync()
+.then(async (result) => {
+  console.log('Done syncing, generating data...')
+  var step = ENTRIES / STEP_COUNT
+  for (var i = 0; i < STEP_COUNT; i++) {
+    var products = [];
+    console.log('Starting step', i);
+    for (var j = (i * step) + 1; j <= (i + 1) * step; j++) {
+
+      products.push({product: j, productName: ''});
+    }
+    console.log('creating products...')
+    await Product.bulkCreate(products)
+    .then(async (result)  => {
+      console.log('Generating image objects...')
+      var images = dataGenerator(i * step, (i + 1) * step);
+      console.log('creating images...')
+      await Image.bulkCreate(images);
+    })
+  }
+  await sequelize.close();
+  console.log('finished');
+});
