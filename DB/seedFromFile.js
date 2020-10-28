d5cf84a7239d0337948eb6010cf0f801143c0c0d
Luke Eastman <LukeAEastman@gmail.com>
2020-10-28 14:47:09 -0700

    change environment variable names to match

--- DB/seedFromFile.js
+++ DB/seedFromFile.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PW,
+  password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT
 });
 
